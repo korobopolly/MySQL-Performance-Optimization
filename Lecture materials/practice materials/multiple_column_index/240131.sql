752f4282918af18f8f2c4be317e361419c401bdc
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2024-02-01 17:08:55 +0900

    HISTOGRAM 분석
    ANALYZE TABLE A UPDATE HISTOGRAM ON a1,a2 WITH 100 BUCKETS

--- Lecture materials/practice materials/multiple_column_index/240131.sql
+++ Lecture materials/practice materials/multiple_column_index/240131.sql
@@ -14,7 +14,7 @@ WHERE customer_id = 3905
 ORDER BY order_date DESC
 LIMIT 10;
 
-EXPLAIN  SELECT * FROM orders
+EXPLAIN SELECT * FROM orders
 WHERE customer_id = 3905
   AND order_date > '2023-11-04'
 ORDER BY order_date DESC
@@ -25,3 +25,10 @@ DROP INDEX idx_order_date_customer_id ON orders;
 CREATE INDEX idx_order_date_customer_id ON orders(order_date, customer_id);
 
 CREATE INDEX idx_customer_id_order_date ON orders(customer_id, order_date);
+
+ANALYZE TABLE orders UPDATE HISTOGRAM ON customer_id, status WITH 100 BUCKETS;
+
+USE information_schema;
+
+SELECT * FROM COLUMN_STATISTICS
+ WHERE TABLE_NAME = 'orders';
