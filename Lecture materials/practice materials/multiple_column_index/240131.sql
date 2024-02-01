7c69c4cc89972670ca2cd57f11d16310b7d6b824
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2024-02-01 11:08:56 +0900

    Multiple-Column Indexes For Optimization
    Multiple-Column Indexes For Optimization

---
+++ Lecture materials/practice materials/multiple_column_index/240131.sql
@@ -0,0 +1,27 @@
+CREATE TABLE orders (
+order_id INT AUTO_INCREMENT,
+customer_id INT,
+order_date DATE,
+product_id INT,
+quantity INT,
+status VARCHAR(50),
+PRIMARY KEY (order_id)
+);
+
+SELECT * FROM orders
+WHERE customer_id = 3905
+  AND order_date > '2023-11-04'
+ORDER BY order_date DESC
+LIMIT 10;
+
+EXPLAIN  SELECT * FROM orders
+WHERE customer_id = 3905
+  AND order_date > '2023-11-04'
+ORDER BY order_date DESC
+LIMIT 10;
+
+DROP INDEX idx_order_date_customer_id ON orders;
+
+CREATE INDEX idx_order_date_customer_id ON orders(order_date, customer_id);
+
+CREATE INDEX idx_customer_id_order_date ON orders(customer_id, order_date);
